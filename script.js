a24f829a6990c2b95ccbbe40cc641ebe63fed574
Sam26 <samueltjenkin@gmail.com>
2023-04-21 10:30:26 +0930

    make form for players to enter their name

--- script.js
+++ script.js
@@ -5,9 +5,11 @@
 // if the tagNamae is 'DIV' continue
 // if playerTurn counter modulo 2 === 0 it's player 1 turn (red) else player 2 turn (blue)
 // increase counter variable to change turn after a click
+
 var mainTag = document.querySelector('main');
 var playerTurn = 0;
-mainTag.addEventListener('click', function(event) {
+
+function handleClick(event) {
     var singleDiv = event.target;
     if (singleDiv.tagName === 'DIV') {
         if ( playerTurn % 2 === 0 ) {
@@ -18,9 +20,6 @@ mainTag.addEventListener('click', function(event) {
         playerTurn++;
     }
 
-
-    
-
 // var to find all the divs on the page
 // if 3 in a row for either colour that player wins
     var allDivs = document.querySelectorAll('div');
@@ -29,34 +28,42 @@ mainTag.addEventListener('click', function(event) {
         allDivs[0].className = 'player-one-wins';
         allDivs[1].className = 'player-one-wins';
         allDivs[2].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[3].className === 'player-one' && allDivs[4].className === 'player-one' && allDivs[5].className === 'player-one') {
         allDivs[3].className = 'player-one-wins';
         allDivs[4].className = 'player-one-wins';
         allDivs[5].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[6].className === 'player-one' && allDivs[7].className === 'player-one' && allDivs[8].className === 'player-one') {
         allDivs[6].className = 'player-one-wins';
         allDivs[7].className = 'player-one-wins';
         allDivs[8].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[0].className === 'player-one' && allDivs[3].className === 'player-one' && allDivs[6].className === 'player-one') {
         allDivs[0].className = 'player-one-wins';
         allDivs[3].className = 'player-one-wins';
         allDivs[6].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[1].className === 'player-one' && allDivs[4].className === 'player-one' && allDivs[7].className === 'player-one') {
         allDivs[1].className = 'player-one-wins';
         allDivs[4].className = 'player-one-wins';
         allDivs[7].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[2].className === 'player-one' && allDivs[5].className === 'player-one' && allDivs[8].className === 'player-one') {
         allDivs[2].className = 'player-one-wins';
         allDivs[5].className = 'player-one-wins';
         allDivs[8].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[0].className === 'player-one' && allDivs[4].className === 'player-one' && allDivs[8].className === 'player-one') {
         allDivs[0].className = 'player-one-wins';
         allDivs[4].className = 'player-one-wins';
         allDivs[8].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[2].className === 'player-one' && allDivs[4].className === 'player-one' && allDivs[6].className === 'player-one') {
         allDivs[2].className = 'player-one-wins';
         allDivs[4].className = 'player-one-wins';
         allDivs[6].className = 'player-one-wins';
+        mainTag.removeEventListener('click', handleClick);
     }
 
 
@@ -64,33 +71,77 @@ mainTag.addEventListener('click', function(event) {
         allDivs[0].className = 'player-two-wins';
         allDivs[1].className = 'player-two-wins';
         allDivs[2].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[3].className === 'player-two' && allDivs[4].className === 'player-two' && allDivs[5].className === 'player-two') {
         allDivs[3].className = 'player-two-wins';
         allDivs[4].className = 'player-two-wins';
         allDivs[5].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[6].className === 'player-two' && allDivs[7].className === 'player-two' && allDivs[8].className === 'player-two') {
         allDivs[6].className = 'player-two-wins';
         allDivs[7].className = 'player-two-wins';
         allDivs[8].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[0].className === 'player-two' && allDivs[3].className === 'player-two' && allDivs[6].className === 'player-two') {
         allDivs[0].className = 'player-two-wins';
         allDivs[3].className = 'player-two-wins';
         allDivs[6].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[1].className === 'player-two' && allDivs[4].className === 'player-two' && allDivs[7].className === 'player-two') {
         allDivs[1].className = 'player-two-wins';
         allDivs[4].className = 'player-two-wins';
         allDivs[7].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[2].className === 'player-two' && allDivs[5].className === 'player-two' && allDivs[8].className === 'player-two') {
         allDivs[2].className = 'player-two-wins';
         allDivs[5].className = 'player-two-wins';
         allDivs[8].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[0].className === 'player-two' && allDivs[4].className === 'player-two' && allDivs[8].className === 'player-two') {
         allDivs[0].className = 'player-two-wins';
         allDivs[4].className = 'player-two-wins';
         allDivs[8].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     } else if (allDivs[2].className === 'player-two' && allDivs[4].className === 'player-two' && allDivs[6].className === 'player-two') {
         allDivs[2].className = 'player-two-wins';
         allDivs[4].className = 'player-two-wins';
         allDivs[6].className = 'player-two-wins';
+        mainTag.removeEventListener('click', handleClick);
     }
-})
+}
+
+mainTag.addEventListener('click', handleClick);
+
+
+
+// form to select player names
+var playerOneName = document.querySelector('#player-one-name');
+var playerTwoName = document.querySelector('#player-two-name');
+var submitPlayerOneName = document.querySelector('#submit-player-one-name');
+var submitPlayerTwoName = document.querySelector('#submit-player-two-name');
+var playerX = document.querySelector('#x');
+var playerO = document.querySelector('#o');
+
+submitPlayerOneName.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (playerOneName.value.trim() !== '') {
+        playerX.textContent = playerOneName.value + ' (X)';
+    }
+
+    playerOneName.value = '';
+});
+
+submitPlayerTwoName.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (playerTwoName.value.trim() !== '') {
+        playerO.textContent = playerTwoName.value + ' (O)';
+    }
+
+    playerTwoName.value = '';
+});
+
+
+// make instead of colour just the text letters X & O so that i can animate.
+// make a resart game button
