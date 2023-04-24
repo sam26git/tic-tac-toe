eadd3cb0c1d3c010c6d009331345b836e6576393
Sam26 <samueltjenkin@gmail.com>
2023-04-24 10:43:07 +0930

    tidy up html, css and js files readability

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// form to select player names
+// Input custom player names
+// universal variables declared for input, button & h2 elements with specific ids (for each player)
+// listen for click on submit button (& prevent default)
+// trim & change h2 textContent to input value (if not empty)
 var playerOneName = document.querySelector('#player-one-name');
 var playerTwoName = document.querySelector('#player-two-name');
 var submitPlayerOneName = document.querySelector('#submit-player-one-name');
@@ -6,52 +9,44 @@ var submitPlayerTwoName = document.querySelector('#submit-player-two-name');
 var playerX = document.querySelector('#x');
 var playerO = document.querySelector('#o');
 
-
-
 submitPlayerOneName.addEventListener('click', function(event) {
     event.preventDefault();
-
     if (playerOneName.value.trim() !== '') {
-        playerX.textContent = playerOneName.value;
+        playerX.textContent = playerOneName.value.trim();
     }
-
     playerOneName.value = '';
 });
 
 submitPlayerTwoName.addEventListener('click', function(event) {
     event.preventDefault();
-
     if (playerTwoName.value.trim() !== '') {
-        playerO.textContent = playerTwoName.value;
+        playerO.textContent = playerTwoName.value.trim();
     }
-
     playerTwoName.value = '';
 });
 
 
-// var storing the main tag (divs container)
-// counter variable to keep track of player turn
-// vars for player X and player O, all the divs, announcing winner, round number and play again button.
-// event listener to listen for a click on mainTag
-// event.target to target a single div
-// if the tagNamae is 'DIV' && tile not clicked on before continue
-// if playerTurn counter modulo 2 === 0 it's player 1 turn else player 2 turn
-// increase counter variable to change turn after a click
+// universal variables declared for main element, all div elements & h2 element with specific id, counter variable for player turn and variablex for player tokens
 var mainTag = document.querySelector('main');
+var allDivs = document.querySelectorAll('div');
+var announceWinner = document.querySelector('#announce-winner');
 var playerTurn = 0;
 var cross = 'X';
 var naught = 'O'
-var allDivs = document.querySelectorAll('div');
-var announceWinner = document.querySelector('#announce-winner');
 
- // keep track of score until page reloaded and announce draw variables
- var trackPlayerOneWins = document.querySelector('#track-player-one-wins');
- var trackPlayerOneDraws = document.querySelector('#track-player-one-draws');
- var trackPlayerOneLoses = document.querySelector('#track-player-one-loses');
- var trackPlayerTwoWins = document.querySelector('#track-player-two-wins');
- var trackPlayerTwoDraws = document.querySelector('#track-player-two-draws');
- var trackPlayerTwoLoses = document.querySelector('#track-player-two-loses');
+// universal variables declared keep track of each players score
+var trackPlayerOneWins = document.querySelector('#track-player-one-wins');
+var trackPlayerOneDraws = document.querySelector('#track-player-one-draws');
+var trackPlayerOneLoses = document.querySelector('#track-player-one-loses');
+var trackPlayerTwoWins = document.querySelector('#track-player-two-wins');
+var trackPlayerTwoDraws = document.querySelector('#track-player-two-draws');
+var trackPlayerTwoLoses = document.querySelector('#track-player-two-loses');
 
+
+// Function to be used for responding to player click on a div element (when added to an event listener)
+// function scoped variable for selecting event.target (will be a singular div)
+// if click is on a div and its textContent is empty then if player turn is divisible by 2, then player 1's turn and a cross token added to textContent of clicked div, else player 2's turn and a naught added to textContent of clicked div
+// increase playerTurn (counter variable by 1)
 function handleClick(event) {
     var singleDiv = event.target;
     if (singleDiv.tagName === 'DIV' && singleDiv.textContent === '') {
@@ -63,8 +58,13 @@ function handleClick(event) {
         playerTurn++;
     }
 
+    // if 3-in-a-row for either player that player wins
+    // change background colour for the tiles that are 3-in-a-row
+    // remove event listener once a player wins (and game is over)
+    // accounce who won with their name
+    // show play again button and remove reset board button
 
-// if 3 in a row for specific player that player wins
+    // if player one wins
     if (allDivs[0].textContent === cross && allDivs[1].textContent === cross && allDivs[2].textContent === cross) {
         allDivs[0].classList.add('player-one-wins');
         allDivs[1].classList.add('player-one-wins');
@@ -131,7 +131,7 @@ function handleClick(event) {
         restartBtn[1].classList.toggle('hidden');
     }
 
-
+    // if player two wins
     if (allDivs[0].textContent === naught && allDivs[1].textContent === naught && allDivs[2].textContent === naught) {
         allDivs[0].classList.add('player-two-wins');
         allDivs[1].classList.add('player-two-wins');
@@ -199,7 +199,14 @@ function handleClick(event) {
     }
 
 
-    // keep track of score until page reloaded and announce draw variables
+    // if a draw & keeping track of player scores
+    // if a draw:
+        // remove event lister
+        // announce a draw
+        // show play again button and remove reset button
+        // add 1 to the draw count for each player
+    // if a player wins:
+        // add 1 to the win count for the winner and add 1 to the lose count for the loser
     if (playerTurn === 9  && announceWinner.textContent === '') {
         mainTag.removeEventListener('click', handleClick);
         announceWinner.textContent = `It was a draw!`;
@@ -217,15 +224,23 @@ function handleClick(event) {
 }
 
 
-// adding event listener to clicks on the divs
+// adding the event listener with the type 'click' to listen for a click on the 'main' element (and enter previously declared function (handleClick) as the listener)
 mainTag.addEventListener('click', handleClick);
 
 
-// make a resart game button and keep scores
+// Buttons for playing another round once the game ends (win, lose or draw), resetting the board if a mistake is made and restarting the game (universal variables declared to cover all buttons)
+// a universal variable for keeping track of the round number
 var restartBtn = document.querySelectorAll('.restart-btn');
-var roundNum = document.querySelector('#round-number');
 var resetGame = document.querySelector('#reset-game');
+var roundNum = document.querySelector('#round-number');
 
+// play again button:
+    // wipes the board by reseting textContent and classes for all divs
+    // resets announceWinner textContent
+    // adds back the event listener on the main element
+    // resets playerTurn counter variable
+    // adds 1 to the round number
+    // hides play again button and shows reset board button
 restartBtn[0].addEventListener('click', function() {
     for (let i = 0; i < allDivs.length; i++) {
         allDivs[i].textContent = '';
@@ -240,6 +255,8 @@ restartBtn[0].addEventListener('click', function() {
     restartBtn[1].classList.toggle('hidden');
 });
 
+// reset board button:
+    // same as above but does not add to the round number or toggle buttons display
 restartBtn[1].addEventListener('click', function() {
     for (let i = 0; i < allDivs.length; i++) {
         allDivs[i].textContent = '';
@@ -249,11 +266,14 @@ restartBtn[1].addEventListener('click', function() {
     announceWinner.textContent = '';
     mainTag.addEventListener('click', handleClick);
     playerTurn = 0;
-    if (!restartBtn[0].classList.contains('hidden')) {
-        restartBtn[0].classList.toggle('hidden');
-    }
 });
 
+// reset board button:
+    // same as above 
+    // reset round number back to 1
+    // reset all player scores back to 0
+    // reset player names to default
+    // if play again button showing and reset board button not showing (i.e. at the end of a round) when reset button clicked, hide play again button and show reset button.
 resetGame.addEventListener('click', function() {
     for (let i = 0; i < allDivs.length; i++) {
         allDivs[i].textContent = '';
@@ -263,9 +283,6 @@ resetGame.addEventListener('click', function() {
     announceWinner.textContent = '';
     mainTag.addEventListener('click', handleClick);
     playerTurn = 0;
-    if (!restartBtn[0].classList.contains('hidden')) {
-        restartBtn[0].classList.toggle('hidden');
-    }
     roundNum.textContent = 1;
     trackPlayerOneWins.textContent = '0';
     trackPlayerOneDraws.textContent = '0';
@@ -275,6 +292,9 @@ resetGame.addEventListener('click', function() {
     trackPlayerTwoLoses.textContent = '0';
     playerX.textContent = 'Player 1';
     playerO.textContent = 'Player 2';
+    if (!restartBtn[0].classList.contains('hidden')) {
+        restartBtn[0].classList.toggle('hidden');
+    }
     if (restartBtn[1].classList.contains('hidden')) {
         restartBtn[1].classList.toggle('hidden');
     }
